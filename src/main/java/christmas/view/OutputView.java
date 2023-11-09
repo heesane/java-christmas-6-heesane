a6a171bb35c3906e669d8a353b657fae2504bffd
heesane <tees3359@gmail.com>
2023-11-09 21:30:55 +0900

    feat : OutputView

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,53 @@
+package christmas.view;
+
+import christmas.view.constant.ViewMessages;
+
+public class OutputView {
+    private void printConstantMessage(String message){
+        System.out.println(message);
+    }
+    private void printWhiteSpace(){
+        System.out.println();
+    }
+    public void printWelcome(){
+        printConstantMessage(ViewMessages.WELCOME_MESSAGE.getMessage());
+    }
+    public void printRequestReservationDate(){
+        printConstantMessage(ViewMessages.REQUEST_RESERVATION_DATE_MESSAGE.getMessage());
+    }
+    public void printRequestMenuAndAmount(){
+        printConstantMessage(ViewMessages.REQUEST_MENU_AND_AMOUNT_MESSAGE.getMessage());
+    }
+    public void printEventPreview(){
+        printWhiteSpace();
+        printConstantMessage(ViewMessages.EVENT_PREVIEW_MESSAGE.getMessage());
+    }
+    public void printOrderList(){
+        printWhiteSpace();
+        printConstantMessage(ViewMessages.ORDER_LIST_MESSAGE.getMessage());
+    }
+    public void printTotalPrice(){
+        printWhiteSpace();
+        printConstantMessage(ViewMessages.TOTAL_PRICE_MESSAGE.getMessage());
+    }
+    public void printFreeGiftList(){
+        printWhiteSpace();
+        printConstantMessage(ViewMessages.FREE_GIFT_LIST_MESSAGE.getMessage());
+    }
+    public void printEventMessage(){
+        printWhiteSpace();
+        printConstantMessage(ViewMessages.EVENT_MESSAGE.getMessage());
+    }
+    public void printDiscount(){
+        printWhiteSpace();
+        printConstantMessage(ViewMessages.DISCOUNT_MESSAGE.getMessage());
+    }
+    public void printTotalPriceAfterDiscount(){
+        printWhiteSpace();
+        printConstantMessage(ViewMessages.TOTAL_PRICE_AFTER_DISCOUNT_MESSAGE.getMessage());
+    }
+    public void printBadgeMessage(){
+        printWhiteSpace();
+        printConstantMessage(ViewMessages.BADGE_MESSAGE.getMessage());
+    }
+}
