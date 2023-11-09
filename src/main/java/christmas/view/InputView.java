fbb7a8d06ff6dfbb6aaf3b88151adbde6911d7eb
heesane <tees3359@gmail.com>
2023-11-09 21:31:07 +0900

    feat : InputView

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,12 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputReservationDay(){
+        return Console.readLine();
+    }
+    public String inputMenuAndAmount(){
+        return Console.readLine();
+    }
+}
