86e63895cc5bd48e51f6d8e997579a403ca3c48e
heesane <tees3359@gmail.com>
2023-11-09 20:50:13 +0900

    feat : Appetizers Enum Class

---
+++ src/main/java/christmas/Enum/Appetizers.java
@@ -0,0 +1,17 @@
+package christmas.Enum;
+
+public enum Appetizers {
+    AL_SONG_SOUP("알송이수프","6,000",6000),
+    TAPAS("타파스","5,500",5500),
+    CAESAR_SALAD("시저샐러드","8,000",8000);
+
+    private String menuName;
+    private String stringPrice;
+    private Integer intPrice;
+
+    Appetizers(String menuName, String stringPrice, Integer intPrice){
+        this.menuName = menuName;
+        this.stringPrice = stringPrice;
+        this.intPrice = intPrice;
+    }
+}
