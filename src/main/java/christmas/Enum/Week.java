880ba804a6c089acd55f944f19c5916e0388ae1c
heesane <tees3359@gmail.com>
2023-11-09 21:00:17 +0900

    feat : Week Enum Class

---
+++ src/main/java/christmas/Enum/Week.java
@@ -0,0 +1,19 @@
+package christmas.Enum;
+
+public enum Week {
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6),
+    SUNDAY(7);
+
+    private Integer day;
+    Week(Integer day){
+        this.day = day;
+    }
+    public Integer getDay() {
+        return day;
+    }
+}
