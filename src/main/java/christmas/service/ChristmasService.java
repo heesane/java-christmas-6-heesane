cdaf7de054dadc5b39582ce5331cd07b06eab688
heesane <tees3359@gmail.com>
2023-11-10 17:53:48 +0900

    feat : Service에서 Injection을 통해 객체 관리

--- src/main/java/christmas/service/ChristmasService.java
+++ src/main/java/christmas/service/ChristmasService.java
@@ -1,14 +1,22 @@
 package christmas.service;
 
+import christmas.model.OrderInfo;
 import christmas.model.Reservation;
+import christmas.parser.ParserOrderInfo;
 import christmas.parser.ParserReservation;
 
 public class ChristmasService {
-    private final ParserReservation parse;
-    public ChristmasService(ParserReservation parse){
-        this.parse = parse;
+    private final ParserReservation parserReservation;
+    private final ParserOrderInfo parserOrderInfo;
+    public ChristmasService(ParserReservation parserReservation, ParserOrderInfo parserOrderInfo){
+        this.parserReservation = parserReservation;
+        this.parserOrderInfo = parserOrderInfo;
     }
     public Reservation makeReservation(String reservationDay) {
-        return parse.parseReservation(reservationDay);
+        return parserReservation.parseReservation(reservationDay);
+    }
+
+    public OrderInfo makeOrderInfo(String orderList){
+        return parserOrderInfo.parserOrderInfo(orderList);
     }
 }
