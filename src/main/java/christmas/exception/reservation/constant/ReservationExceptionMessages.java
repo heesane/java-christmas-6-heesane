a7fa597558ef5c031db125029848cb75f657cbfa
heesane <tees3359@gmail.com>
2023-11-10 16:39:36 +0900

    feat : Validator에서 사용할 ErrorMessage 정의

---
+++ src/main/java/christmas/exception/reservation/constant/ReservationExceptionMessages.java
@@ -0,0 +1,13 @@
+package christmas.exception.reservation.constant;
+
+public enum ReservationExceptionMessages {
+    INVALID_RESERVATION_DATE("유효하지 않은 날짜입니다. 다시 입력해주세요.");
+    private final String message;
+    private final String PREFIX = "[ERROR] ";
+    ReservationExceptionMessages(String message){
+        this.message = message;
+    }
+    public String getMessage(){
+        return PREFIX + message;
+    }
+}
