7deee0fc7af78e70916fa51ebea22108ba7facbc
heesane <tees3359@gmail.com>
2023-11-11 23:51:06 +0900

    feat : Menu 파악과 HashMap을 통한 가격 조회

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -21,23 +21,43 @@ public class Menu {
     }
 
     public boolean isAppetizer(String menu){
-        return appetizers.contains(menu);
+        return checkMenu(appetizers,menu);
     }
     public boolean isDessert(String menu){
-        return desserts.contains(menu);
+        return checkMenu(desserts,menu);
     }
     public boolean isMainDish(String menu){
-        return mainDishes.contains(menu);
+        return checkMenu(mainDishes,menu);
     }
     public boolean isDrink(String menu){
-        return drinks.contains(menu);
+        return checkMenu(drinks,menu);
     }
-    public List<String> allMenus(){
-        List<String> allMenus = new ArrayList<>();
+    public List<HashMap<String,Integer>> allMenus(){
+        List<HashMap<String,Integer>> allMenus = new ArrayList<>();
         allMenus.addAll(appetizers);
         allMenus.addAll(desserts);
         allMenus.addAll(mainDishes);
         allMenus.addAll(drinks);
         return allMenus;
+    }
+    public Integer getMenuPrice(String menu){
+        List<HashMap<String,Integer>> allMenus = allMenus();
+        for(HashMap<String,Integer> menuHashMap : allMenus){
+            if(menuHashMap.containsKey(menu)){
+                return menuHashMap.get(menu);
+            }
+        }
+        return 0;
+    }
+    public boolean isMenu(String menu){
+        return checkMenu(appetizers,menu) || checkMenu(desserts,menu) || checkMenu(mainDishes,menu) || checkMenu(drinks,menu);
+    }
+    private boolean checkMenu(List<HashMap<String,Integer>> menuList, String menu){
+        for(HashMap<String,Integer> menuHashMap : menuList){
+            if(menuHashMap.containsKey(menu)){
+                return true;
+            }
+        }
+        return false;
     }
 }
