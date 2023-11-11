235a93616f8bceef2d137f4509a55584af935898
heesane <tees3359@gmail.com>
2023-11-11 23:53:34 +0900

    feat : 의존성 주입을 통한 private 메서드 사용

--- src/main/java/christmas/parser/ParserReservation.java
+++ src/main/java/christmas/parser/ParserReservation.java
@@ -4,26 +4,25 @@ import christmas.exception.reservation.InvalidReservationDateException;
 import christmas.model.Reservation;
 import christmas.validator.ReservationValidator;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public class ParserReservation {
     private static ReservationValidator validator;
-    private InputView input;
-
-    public ParserReservation(InputView input){
+    private final InputView input;
+    private final OutputView output;
+    public ParserReservation(InputView input, OutputView output){
         validator = new ReservationValidator();
         this.input = input;
+        this.output = output;
     }
     public Reservation parseReservation(String reservationDay){
         try{
             validator.isValidReservationDay(reservationDay);
         }catch(InvalidReservationDateException e){
-            printErrorMessage(e.getMessage());
+            output.printExceptionMessage(e);
             return parseReservation(input.inputReservationDay());
         }
 
         return new Reservation(Integer.parseInt(reservationDay));
-    }
-    private void printErrorMessage(String ErrorMessage){
-        System.out.println(ErrorMessage);
     }
 }
