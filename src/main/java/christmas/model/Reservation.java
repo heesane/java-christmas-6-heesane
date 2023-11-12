5137153ec05d0d8938acebd952b894a05297c675
heesane <tees3359@gmail.com>
2023-11-12 18:39:12 +0900

    feat : Reservation toString Override

--- src/main/java/christmas/model/Reservation.java
+++ src/main/java/christmas/model/Reservation.java
@@ -1,4 +1,8 @@
 package christmas.model;
 
 public record Reservation(Integer reservationDay) {
+    @Override
+    public String toString(){
+        return "12월 "+reservationDay.toString()+"일";
+    }
 }
