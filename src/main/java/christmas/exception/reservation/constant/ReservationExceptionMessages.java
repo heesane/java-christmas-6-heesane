2203ca1d23fc078a86eb16c49a3e3871edb78183
heesane <tees3359@gmail.com>
2023-11-12 20:42:35 +0900

    fix : INVALID_RESERVATION_DATE 오타 수정

--- src/main/java/christmas/exception/reservation/constant/ReservationExceptionMessages.java
+++ src/main/java/christmas/exception/reservation/constant/ReservationExceptionMessages.java
@@ -1,7 +1,7 @@
 package christmas.exception.reservation.constant;
 
 public enum ReservationExceptionMessages {
-    INVALID_RESERVATION_DATE("유효하지 않은 날짜입니다. 다시 입력해주세요.");
+    INVALID_RESERVATION_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     private final String message;
     private final String PREFIX = "[ERROR] ";
     ReservationExceptionMessages(String message){
